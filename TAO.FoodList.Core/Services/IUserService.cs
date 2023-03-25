941d186a210962f3b1c078d5edd90ba293cc6c2f
Moongazing <72943708+Moongazing@users.noreply.github.com>
2023-03-26 01:55:49 +0300

    Adding UserService

---
+++ TAO.FoodList.Core/Services/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TAO.FoodList.Core.Dtos;
+using TAO.FoodList.Core.Entities.Auth;
+using TAO.FoodList.Shared.Dtos;
+
+namespace TAO.FoodList.Core.Services
+{
+    public interface IUserService
+    {
+        Task<Response<UserAppDto>> CreateUserAsync(CreateUserDto createUserDto);
+        Task<Response<UserAppDto>> GetUserByName(string userName);
+    }
+}
