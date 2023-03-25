6139d2ae58a249e37a48620c8191e1cc0891843d
Moongazing <72943708+Moongazing@users.noreply.github.com>
2023-03-25 20:55:45 +0300

    Adding IAuthenticationService

--- TAO.FoodList.Core/Services/IAuthenticationService.cs
+++ TAO.FoodList.Core/Services/IAuthenticationService.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TAO.FoodList.Core.Dtos;
+using TAO.FoodList.Shared.Dtos;
 
 namespace TAO.FoodList.Core.Services
 {
     public interface IAuthenticationService
     {
-
+        Task<Response<TokenDto>>CreateTokenAsync(LoginDto loginDto);
+        Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
+        Task<Response<NoDataDto>> RemoveRefreshToken(string refreshToken);
+        Task<Response<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto);
     }
 }
