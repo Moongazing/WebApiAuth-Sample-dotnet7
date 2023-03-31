3f951f7cc5d8256c79e0ae7075fc467bb682afc5
Moongazing <72943708+Moongazing@users.noreply.github.com>
2023-03-31 10:58:30 +0300

    Adding UserService

---
+++ TAO.FoodList.Service/Services/UserService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TAO.FoodList.Core.Dtos;
+using TAO.FoodList.Core.Entities.Auth;
+using TAO.FoodList.Core.Services;
+using TAO.FoodList.Service.AutoMapper;
+using TAO.FoodList.Shared.Dtos;
+
+namespace TAO.FoodList.Service.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<UserApp> _userManager;
+        public UserService(UserManager<UserApp> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<Response<UserAppDto>> CreateUserAsync(CreateUserDto createUserDto)
+        {
+            var user = new UserApp
+            {
+                Email = createUserDto.Email,
+                UserName = createUserDto.UserName
+            };
+
+            var result = await _userManager.CreateAsync(user,createUserDto.Password);
+            if (!result.Succeeded)
+            {
+                var errors = result.Errors.Select(x => x.Description).ToList();
+                return Response<UserAppDto>.Fail(400, new ErrorDto(errors, true));
+            }
+            return Response<UserAppDto>.Success(ObjectMapper.Mapper.Map<UserAppDto>(user), 200);
+        }
+
+        public async Task<Response<UserAppDto>> GetUserByName(string userName)
+        {
+           var user = await _userManager.FindByNameAsync(userName);
+            if (user== null)
+            {
+                return Response<UserAppDto>.Fail(400, "Username not found.", true);
+            }
+            return Response<UserAppDto>.Success(ObjectMapper.Mapper.Map<UserAppDto>(user), 200);
+
+        }
+    }
+}
