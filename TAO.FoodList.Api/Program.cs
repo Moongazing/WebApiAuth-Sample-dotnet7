18f1eca8ed4865168c293efd6463aca79a3a9c26
Moongazing <72943708+Moongazing@users.noreply.github.com>
2023-03-31 11:35:18 +0300

    Adding DI Register, Token validate

--- TAO.FoodList.Api/Program.cs
+++ TAO.FoodList.Api/Program.cs
@@ -1,4 +1,15 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using TAO.FoodList.Core.Configuration;
+using TAO.FoodList.Core.Entities.Auth;
+using TAO.FoodList.Core.Repositories;
+using TAO.FoodList.Core.Services;
+using TAO.FoodList.Core.UnitOfWork;
+using TAO.FoodList.Data.DbContexts;
+using TAO.FoodList.Data.Repositories;
+using TAO.FoodList.Data.UnitOfWorks;
+using TAO.FoodList.Service.Services;
 using TAO.FoodList.Shared.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,8 +20,63 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region DI Register
+
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped(typeof(IGenericService<,>), typeof(GenericService<,>));
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+#endregion
+
+
 builder.Services.Configure<CustomTokenOption>(builder.Configuration.GetSection("TokenOptions"));
+
+var tokenOptions = builder.Configuration.GetSection("TokenOptions").Get<CustomTokenOption>();
+
 builder.Services.Configure<List<Client>>(builder.Configuration.GetSection("Clients"));
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"), sqlOptions =>
+     {
+
+         sqlOptions.MigrationsAssembly("TAO.FoodList.Data");
+
+     });
+
+});
+builder.Services.AddIdentity<UserApp, IdentityRole>(options=>
+{
+    options.User.RequireUniqueEmail = true;
+    options.Password.RequireNonAlphanumeric = false;
+
+}).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme=JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme=JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options=>
+{
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters() 
+    { 
+       ValidIssuer = tokenOptions.Issuer,
+       ValidAudience = tokenOptions.Audience[0],
+       IssuerSigningKey = SignInService.GetSymmetricSecurityKey(tokenOptions.SecurityKey),
+       ValidateIssuerSigningKey = true,
+       ValidateAudience = true,
+       ValidateIssuer = true,
+       ValidateLifetime = true,
+
+    };
+});
+
+
+
+
 
 var app = builder.Build();
 
